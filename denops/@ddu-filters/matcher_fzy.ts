e38dced5b4a636345e028aec667962c1c13ced04
Bakudankun <bakudankun@gmail.com>
2023-03-09 23:42:18 +0900

    Properly calculate highlight positions

--- denops/@ddu-filters/matcher_fzy.ts
+++ denops/@ddu-filters/matcher_fzy.ts
@@ -1,8 +1,4 @@
-import {
-  hasMatch,
-  positions,
-  score,
-} from "./fzy.js";
+import { hasMatch, positions, score } from "./fzy.js";
 import {
   BaseFilter,
   DduItem,
@@ -17,6 +13,10 @@ type SortItem = {
   score: number;
   item: DduItem;
 };
+
+function utf8Length(str: string): number {
+  return (new TextEncoder().encode(str)).length;
+}
 
 export class Filter extends BaseFilter<Params> {
   filter(args: {
@@ -43,18 +43,24 @@ export class Filter extends BaseFilter<Params> {
         c,
       ) => ({ score: score(input, c.word), item: c } as SortItem))
         .sort((a, b) => b.score - a.score).map((c) => {
-          const prev = c.item.highlights?.filter((h) => h.name != "ddu_fzy_hl");
-          const highlights = positions(input, c.item.word).map((p) => ({
-            col: p + 1,
-            type: "abbr",
-            name: "ddu_fzy_hl",
-            "hl_group": args.filterParams.hlGroup,
-            width: 1,
-          }));
-          if (prev) {
-            c.item.highlights = prev.concat(highlights);
-          } else {
-            c.item.highlights = highlights;
+          const hlOffset = c.item.display === undefined
+            ? 0
+            : c.item.display.indexOf(c.item.word);
+          if (hlOffset >= 0) {
+            const display = c.item.display ?? c.item.word;
+            const prev = c.item.highlights?.filter((h) => h.name != "ddu_fzy_hl");
+            const highlights = positions(input, c.item.word).map((p) => ({
+              col: utf8Length(display.substring(0, hlOffset + p)) + 1,
+              type: "abbr",
+              name: "ddu_fzy_hl",
+              "hl_group": args.filterParams.hlGroup,
+              width: 1,
+            }));
+            if (prev) {
+              c.item.highlights = prev.concat(highlights);
+            } else {
+              c.item.highlights = highlights;
+            }
           }
           return c.item;
         }),
